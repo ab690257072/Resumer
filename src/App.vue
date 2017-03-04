fa4847eb429fcdbd82908772b4950db8e8651d55
ab690257072 <690257072@qq.com>
2017-03-04 21:11:52 +0800

    step-2

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,82 @@
 <template>
-<div class="page">
-  <header>
-    <Topbar/>
-  </header>
-  <main>
-    <ResumeEditor/>
-    <ResumePreview/>
-  </main>
-</div>
+  <div class="page">
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
 </template>
 
 <script>
-import './assets/reset.css'
-import 'normalize.css/normalize.css'
+  import './assets/reset.css'
+  import 'normalize.css/normalize.css'
 
-import Topbar from './components/Topbar'
-import ResumeEditor from './components/ResumeEditor'
-import ResumePreview from './components/ResumePreview'
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview'
+  import icons from './assets/icons'
 
-export default {
-  name: 'app',
-  data () {
-    return {
-      text: 'hello world'
+  export default {
+    name: 'app',
+    data() {
+      return {
+        text: 'hello world'
+      }
+    },
+    components: {
+      Topbar,
+      ResumeEditor,
+      ResumePreview
+    },
+    created() {
+      document.body.insertAdjacentHTML('afterbegin', icons);
     }
-  },
-  components: {
-    Topbar,
-    ResumeEditor,
-    ResumePreview
   }
-}
+
 </script>
 
-<style>
+<style lang="less">
   .page {
     height: 100vh;
     display: flex;
     flex-direction: column;
     background: #EAEBEC;
+    >main {
+      flex-grow: 1;
+    }
+    >main {
+      min-width: 1024px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%;
+      align-self: center;
+    }
   }
-  .page > main {
-    flex-grow: 1;
-  }
-  .page > main {
-    min-width: 1024px;
-    max-width: 1440px;
-    margin-top: 16px;
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: space-between;
-    padding: 0 16px;
-    width: 100%;
-    align-self: center;
-  }
+  
   #resumeEditor {
     width: 35%;
     background: #444;
   }
+  
   #resumePreview {
     flex-grow: 1;
     margin-left: 16px;
     background: #777;
   }
+  
+  svg.icon {
+    height: 1em;
+    width: 1em;
+    fill: currentColor;
+    vertical-align: -0.1em;
+    font-size: 16px;
+  }
+
 </style>
--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -2,6 +2,10 @@
   <div id="topbar">
     <div class="warpper">
       <span class="logo">Resume</span>
+      <div class="actions">
+        <button class="primary">保存</button>
+        <button>预览</button>
+      </div>
     </div>
   </div>
 </template>
@@ -9,27 +13,44 @@
   export default {
     name: 'Topbar'
   }
+
 </script>
-<style>
+<style scoped lang="less">
   #topbar {
     background: #fff;
-    box-shadow:  0 1px 3px 0 rgba(0, 0, 0, .25);
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, .25);
     height: 64px;
+    >.warpper {
+      min-width: 1024px;
+      max-width: 1440px;
+      margin: 0 auto;
+      height: 64px;
+    }
+    >.warpper {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 16px;
+    }
+    .logo {
+      font-size: 24px;
+      color: #000;
+    }
   }
-  #topbar > .warpper {
-    min-width: 1024px;
-    max-width: 1440px;
-    margin: 0 auto;
-    height: 64px;
-  }
-  #topbar > .warpper {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 16px;
-  }
-  #topbar .logo {
-    font-size: 24px;
-    color: #000;
+  button {
+    width: 72px;
+    height: 32px;
+    border: none;
+    background: #ddd;
+    font-size: 18px;
+    cursor: pointer;
+    color: #222;
+    &:hover {
+      box-shadow: 1px 1px 1px hsla(0, 0, 0, .5);
+    }
+    &.primary {
+      background: #02af5f;
+      color: #fff;
+    }
   }
 </style>
